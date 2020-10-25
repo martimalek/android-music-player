c94264da5f4f4832fb713150fcc578fb11e10603
martimalek <martimalekdot@hotmail.es>
2020-10-25 18:35:59 +0100

    Basic navigation setup

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { SafeAreaView, StatusBar, StyleSheet, Text } from 'react-native';
+import { StatusBar } from 'react-native';
 
-import { AudioList } from './screens/AudioList';
+import { MainNavigator } from './navigation/MainNavigator';
 
 const App = () => {
 
     return (
         <>
             <StatusBar barStyle="dark-content" />
-            <SafeAreaView style={styles.container}>
-                <Text style={styles.title}>MUSIC</Text>
-                <AudioList />
-            </SafeAreaView>
+            <MainNavigator />
         </>
     );
 };
-
-const styles = StyleSheet.create({
-    title: {
-        fontSize: 20,
-        padding: 10,
-        color: 'white',
-    },
-    container: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        backgroundColor: '#100B2E',
-    },
-});
 
 export default App;
