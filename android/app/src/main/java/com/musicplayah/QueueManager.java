30b4bb9622262818ecd61e419ef413b2fdb7832b
martimalek <martimalekdot@hotmail.es>
2020-11-20 20:09:46 +0100

    WIP - QueueManager

---
+++ android/app/src/main/java/com/musicplayah/QueueManager.java
@@ -0,0 +1,17 @@
+package com.musicplayah;
+
+import android.content.Context;
+
+public class QueueManager {
+    private static final String TAG = Constants.TAG;
+
+    private List<MediaSessionCompat>
+
+    public QueueManager(
+            MusicProvider musicProvider,
+            Context context,
+
+    ) {
+
+    }
+}
